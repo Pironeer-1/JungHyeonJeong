1b9d26cdf167e4c1e211f458ff3dae628de21072
cathy <cathy02473500@gmail.com>
2024-09-25 12:53:51 +0900

    board 응답규격화

--- week3/src/main/java/com/pironeer/week3/board/controller/BoardController.java
+++ week3/src/main/java/com/pironeer/week3/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.pironeer.week3.board.dto.request.BoardCreateRequest;
 import com.pironeer.week3.board.dto.request.BoardUpdateRequest;
 import com.pironeer.week3.board.dto.response.BoardResponse;
 import com.pironeer.week3.board.service.BoardService;
+import com.pironeer.week3.global.dto.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -22,23 +23,23 @@ public class BoardController {
 
     @PostMapping
     @Operation(summary = "게시물 작성")
-    public ResponseEntity<Long> create(@Valid @RequestBody BoardCreateRequest request){
+    public SuccessResponse<Long> create(@Valid @RequestBody BoardCreateRequest request){
         Long saveId = boardService.save(request);
-        return ResponseEntity.ok().body(saveId);
+        return SuccessResponse.ok(saveId);
     }
 
     @GetMapping("/{topicId}")
     @Operation(summary = "게시물 단건 조회")
-    public  ResponseEntity<BoardResponse> read(@PathVariable("topicId") Long id){
+    public  SuccessResponse<BoardResponse> read(@PathVariable("topicId") Long id){
         BoardResponse response = boardService.findById(id);
-        return ResponseEntity.ok().body(response);
+        return SuccessResponse.ok(response);
     }
 
     @GetMapping
     @Operation(summary = "게시물 전체 조회")
-    public ResponseEntity<List<BoardResponse>> readAll(){
+    public SuccessResponse<List<BoardResponse>> readAll(){
         List<BoardResponse> responses = boardService.findAll();
-        return ResponseEntity.ok().body(responses);
+        return SuccessResponse.ok(responses);
     }
 
     @PutMapping
